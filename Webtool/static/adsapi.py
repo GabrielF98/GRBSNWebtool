91ccdbbb7bfd21445289413469be9b1c28a35d20
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2021-06-22 15:56:32 +0100

    Isolated the Hyperlinks, authors and years

--- Webtool/static/adsapi.py
+++ Webtool/static/adsapi.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 #The requests module
 import requests
@@ -17,27 +18,33 @@ def bibcode_names():
 	conn = get_db_connection()
 	urls = conn.execute('SELECT DISTINCT(PrimarySources) FROM SQLDataGRBSNe')
 	bibcodes = []
+	hyperlinks = [] 
 	for i in urls:
 		
 		if str(i[0])[0:10] == 'https://ui':
 
 			#Split the bibcode into a list by breaking it each time a / appears
 			bibcodes.append(str(i[0]).split('/')[4].replace('%26', '&'))
-
+			hyperlinks.append(str(i[0]))
 		#Skip anything without https://ui
 		else:
 			continue
 	conn.close()
 	
-	return bibcodes
+	return bibcodes, hyperlinks
 
-bibcodes = bibcode_names()
+bibcodes, hyperlinks = bibcode_names()
 print(bibcodes)
 #API Access
 #Code copied from the howto for the ADS API (though some of it is mine too)
 #https://github.com/adsabs/adsabs-dev-api/blob/master/Converting_curl_to_python.ipynb
+
 #Send query to ADS and get data back
 token = 'oLH7C9g5twATAq6yW1PDHIAtAxaXQzNcSj71Az67'
+
+authors = []
+years = []
+
 for i in range(len(bibcodes)):
 
 	bibcode = {"bibcode":[str(bibcodes[i])], "format": "%m %Y"}
@@ -46,10 +53,14 @@ for i in range(len(bibcodes)):
 	                 data=json.dumps(bibcode))
 	print(r.json())
 
+	authors.append(r.json()['export'][:-5])
+	years.append(r.json()['export'][-5:])
 
-
-
-
+#Save the ads_url, name and year in one txt file		
+import csv
+with open('citations.csv', 'w') as f:
+	writer = csv.writer(f, delimiter='\t')
+	writer.writerows(zip(hyperlinks, authors, years))
 
 
 
