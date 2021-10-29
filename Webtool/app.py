bbfa046b965443eee76e799e92e813ae2359bebe
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2021-10-29 20:17:49 +0100

    Minor changes

--- Webtool/app.py
+++ Webtool/app.py
@@ -153,8 +153,7 @@ def home():
 
     #Ok new plan
     #Going to give it a go with the UNION and INTERSECT commands
-    initial_query = ('SELECT GRB, SNe, e_iso, z, T90 FROM SQLDataGRBSNe')
-
+    initial_query = ('SELECT GRB, SNe, e_iso, z, T90 FROM SQLDataGRBSNe GROUP BY GRB')
     data = conn.execute(initial_query).fetchall()
     conn.close()
 
