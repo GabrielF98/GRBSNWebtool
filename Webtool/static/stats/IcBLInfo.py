f40bd19a27e4986210e04ca852cbaec190d2f33e
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2022-02-17 09:37:32 +0000

    Fixed graph

--- Webtool/static/stats/IcBLInfo.py
+++ Webtool/static/stats/IcBLInfo.py
@@ -8,20 +8,21 @@ data = pd.read_csv('tns_Ic-BLs_Feb22.csv')
 redshift = data['Redshift']
 sn_names = data['Name']
 
-
 spec_IcBLs_with_GRBs = ['2016jca', '2017htp', '2017iuk', '2018fip', '2019jrj', '2020bvc', 'AT2019oyw']
 
 #Number of SN events per year with and without GRBs
-event_years_with = [1, 2, 1, 2, 1, 0, 0]
+event_years_with = [1, 2, 1, 2, 1, 1, 0]
 
 event_years_without = [0, 0, 0, 0, 0, 0, 0]
 
-
+#Loop over the names of the Ic-BLs
 for i in sn_names:
-	if i not in spec_IcBLs_with_GRBs:
-		print(i[3:7])
-		print(int(i[3:7])-2016)
-		event_years_without[int(i[3:7])-2016]+=1
+	print(i)
+	print(int(i[3:7])-2016)
+	event_years_without[int(i[3:7])-2016]+=1
+
+for i in range(len(event_years_without)):
+	event_years_without[i] = event_years_without[i]-event_years_with[i]
 
 labels = ['2016', '2017', '2018', '2019', '2020', '2021', 'Feb. 2022']
 width = 0.6
