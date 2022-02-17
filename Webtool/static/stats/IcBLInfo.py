6f024f56fb598e855f9d6498104d1a8fe5cbc46c
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2022-02-17 09:05:28 +0000

    Fixed the graph for the Ic-BLs per year

--- Webtool/static/stats/IcBLInfo.py
+++ Webtool/static/stats/IcBLInfo.py
@@ -3,18 +3,18 @@ import numpy as np
 
 import pandas as pd
 
-data = pd.read_csv('DataonIc-BLs.csv')
+data = pd.read_csv('tns_Ic-BLs_Feb22.csv')
 
 redshift = data['Redshift']
 sn_names = data['Name']
 
 
-spec_IcBLs_with_GRBs = ['2016jca', '2017htp', '2017iuk', '2018fip', '2019jrj', '2020bvc', '2020oi', 'AT2019oyw']
+spec_IcBLs_with_GRBs = ['2016jca', '2017htp', '2017iuk', '2018fip', '2019jrj', '2020bvc', 'AT2019oyw']
 
 #Number of SN events per year with and without GRBs
-event_years_with = [1, 2, 1, 2, 2, 0]
+event_years_with = [1, 2, 1, 2, 1, 0, 0]
 
-event_years_without = [0, 0, 0, 0, 0, 0]
+event_years_without = [0, 0, 0, 0, 0, 0, 0]
 
 
 for i in sn_names:
@@ -23,7 +23,7 @@ for i in sn_names:
 		print(int(i[3:7])-2016)
 		event_years_without[int(i[3:7])-2016]+=1
 
-labels = ['2016', '2017', '2018', '2019', '2020', 'October 2021']
+labels = ['2016', '2017', '2018', '2019', '2020', '2021', 'Feb. 2022']
 width = 0.6
 
 fig, ax = plt.subplots()
@@ -33,6 +33,6 @@ ax.bar(labels, event_years_with, width, color='purple', alpha=0.5, bottom=event_
 
 ax.set_ylabel('IcBLs/year')
 ax.legend()
-plt.yticks([0, 5, 10, 15, 20])
+plt.yticks([0, 5, 10, 15, 20, 25])
 
 plt.savefig('IcBLAssociationsNumberperyear(Bar).pdf') 
