bf24eb2593866f454c50357ad6697a0a458989d3
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2022-04-01 16:33:18 +0100

    Got parser running
    its not working perfectly though

--- Webtool/static/SourceData/parser.py
+++ Webtool/static/SourceData/parser.py
@@ -1,31 +1,44 @@
 import numpy as np
-
-# open text file
-file = r'GRB171205A/GRB171205Axrtlc.txt'
-
-
-f = open(file, 'r')
-f = f.readlines()
-f = np.array(f)
-
-# Loop through the lines to find what we need:
-read_terr = -1
-wt_slew = -1
-wt = -2
-wt_limit = -3
-pc = -4
-pc_limit = -5
-
-# List that will store the index the rows change datatype at
-indices = []
-
-with open
-for i in range(len(f)):
-    print(f[i])
-    if 'WTSLEW' in str(f[i]]):
-
+import os
+eventlist = os.listdir()
+forbidden_folders = ['.DS_Store', 'GoodXRTData']
+for item in eventlist:
+    if os.path.isdir(item):
+        if str(item) in forbidden_folders:
+            eventlist.remove(item)
     else:
+        eventlist.remove(item)
+eventlist = ['GRB171205A-SN2017iuk']
+# Loop over all folders
+for folder in eventlist:
+    with open('newfile.txt', 'w') as fnew: 
+        grb_name = str(list(folder.split('-'))[0])
+        print(grb_name)
+        folder = str(folder)
+        # open text file
+        file = folder+'/'+grb_name+'xrtlc.txt'
 
-        
 
-# with open('test.txt', 'w') as newf:
+        f = open(file, 'r')
+        f = f.readlines()
+        f = np.array(f)
+
+        # Loop through the lines to find what we need:
+        code = 0
+
+        for i in range(len(f)):
+            print(f[i])
+            if 'WTSLEW' in str(f[i]):
+                code = 1
+            elif 'WT' in str(f[i]):
+                code = 2
+            elif 'WT' and 'limit' in str(f[i]):
+                code = 3
+            elif 'PC' in str(f[i]):
+                code = 4
+            elif 'PC' and 'limit' in str(f[i]):
+                code = 3
+            else:
+                code = code
+                to_write = str(f[i])+' '+str(code)
+                fnew.writelines(to_write)
