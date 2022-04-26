836d2d76b7af69b8fe38a8c1bb8e42ad3f448fee
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2022-04-26 13:11:28 +0100

    Scientific notation fixes
    Created a code that converts the capital E to e for the sci notation.

--- Webtool/static/sci-notation-edit.py
+++ Webtool/static/sci-notation-edit.py
@@ -1,5 +1,4 @@
 import sqlite3
-import numpy as np
 
 def get_db_connection():
     conn = sqlite3.connect('Masterbase.db')
@@ -8,17 +7,22 @@ def get_db_connection():
 
 #Connect to the db and get the data
 conn = get_db_connection()
-init_data = conn.execute("SELECT e_iso from SQLDataGRBSNe").fetchall()
+init_data = conn.execute("SELECT GRB, SNe, e_iso from SQLDataGRBSNe").fetchall()
 conn.close()
 
 #Print the data
 new_data = []
-for i in init_data:
-    print(i[0])
-    new_data.append(str(i[0]).replace('E', 'e'))
+for i in range(len(init_data)):
+    k = init_data[i][2]
+    if str(init_data[i][2])!='None':
+        k = str(init_data[i][2]).replace('E', 'e')
+
+
+    a = [i, k]
+    new_data.append(a)
 
 #Update the db
 conn = get_db_connection()
-conn.executemany("UPDATE SQLDataGRBSNe SET e_iso = ?", ((i,) for i in new_data))
+conn.executemany("UPDATE SQLDataGRBSNe SET e_iso = ? WHERE rowid=?", ((i[1], i[0]+1,) for i in new_data))
 conn.commit()
 conn.close()
