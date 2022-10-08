af8103d7d253eab73118cf5429debc54f2e01af6
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2022-10-08 22:12:41 +0100

    Added a check to make sure the readme.txt is there already.

--- Webtool/static/SourceData/markdownreadmes.py
+++ Webtool/static/SourceData/markdownreadmes.py
@@ -18,39 +18,41 @@ folders = ['GRB030329-SN2003dh']
 # Loop over the folders.
 for folder in folders:
 
-	# Open the existing text readme. 
-	with open(folder+'/readme.txt', 'r') as readme:
-		readmelines = readme.readlines()
+	# Check if the readme.txt file exists yet.
+	if 'readme.txt' in os.listdir(folder):
+		# Open the existing text readme. 
+		with open(folder+'/readme.txt', 'r') as readme:
+			readmelines = readme.readlines()
 
 
-	# Locate the limes in the file where the breaks between the files are. 
-	linenos = []
-	for j, line in enumerate(readmelines):
-		if '=========================================================\n' == line:
-			linenos.append(j)
+		# Locate the limes in the file where the breaks between the files are. 
+		linenos = []
+		for j, line in enumerate(readmelines):
+			if '=========================================================\n' == line:
+				linenos.append(j)
 
-	# Create a markdown doc in the correct folder. 
-	doc = Document(folder+"/readme")
-	doc.add_header(readmelines[0].split(' ')[2]) # The first line has the GRB-SN ID. 
+		# Create a markdown doc in the correct folder. 
+		doc = Document(folder+"/readme")
+		doc.add_header(readmelines[0].split(' ')[2]) # The first line has the GRB-SN ID. 
 
-	# Loop over the readme between the ========= marks that divide the files. 
-	for i in range(len(linenos)):
-		doc.add_header((readmelines[linenos[i]+1]).split(' ')[1], 3) # The filename
-		p = doc.add_paragraph('**'+readmelines[linenos[i]+2].split(' ')[0]+'** '+readmelines[linenos[i]+2].split(' ')[1])
-		p.add_paragraphinsert_link = (readmelines[linenos[i]+2].split(' ')[1], readmelines[linenos[i]+2].split(' ')[1]) # The source
-		doc.add_paragraph('**'+(readmelines[linenos[i]+3]).split(' ')[0]+'** '+(readmelines[linenos[i]+3]).split(' ')[1]) # The datatype
+		# Loop over the readme between the ========= marks that divide the files. 
+		for i in range(len(linenos)):
+			doc.add_header((readmelines[linenos[i]+1]).split(' ')[1], 3) # The filename
+			p = doc.add_paragraph('**'+readmelines[linenos[i]+2].split(' ')[0]+'** '+readmelines[linenos[i]+2].split(' ')[1])
+			p.add_paragraphinsert_link = (readmelines[linenos[i]+2].split(' ')[1], readmelines[linenos[i]+2].split(' ')[1]) # The source
+			doc.add_paragraph('**'+(readmelines[linenos[i]+3]).split(':')[0]+':** '+(readmelines[linenos[i]+3]).split(':')[1]) # The datatype
+			
+			# Add a new line for each note.
+			doc.add_paragraph('**Notes**')
+			# Is it the last file
+			# No
+			if i+1!=len(linenos):
+				for note in range(linenos[i+1]-(linenos[i]+5)): # Range from the first note to the last.
+					doc.add_paragraph(readmelines[linenos[i]+5+note]) # Notes
+			# Yes
+			else: # If its the last file keep going until the end of the notes.
+				for note in range(len(readmelines)-(linenos[i]-5)): # Range from the first note to the last.
+					doc.add_paragraph(readmelines[linenos[i]-5+note]) # Notes
 		
-		# Add a new line for each note.
-		doc.add_paragraph('**Notes**')
-		# Is it the last file
-		# No
-		if i+1!=len(linenos):
-			for note in range(linenos[i+1]-(linenos[i]+5)): # Range from the first note to the last.
-				doc.add_paragraph(readmelines[linenos[i]+5+note]) # Notes
-		# Yes
-		else: # If its the last file keep going until the end of the notes.
-			for note in range(len(readmelines)-(linenos[i]-5)): # Range from the first note to the last.
-				doc.add_paragraph(readmelines[linenos[i]-5+note]) # Notes
-	
-	# Create the doc. 
-	doc.output_page()
+		# Create the doc. 
+		doc.output_page()
