c53afd6f4ac8afb4f16f8e035c6c2ab5fb69970d
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2022-10-08 17:46:02 +0100

    Update markdownreadmes.py
    Added a way to get into the correct folder and also a method to access the markdown creation code.

--- Webtool/static/SourceData/markdownreadmes.py
+++ Webtool/static/SourceData/markdownreadmes.py
@@ -1,3 +1,28 @@
-import snakemd
+''' 
+Creates a markdown readme from the txt readmes for the GRB-SN data files. 
+'''
 
-doc = snakemd.create_doc("Title")
+import snakemd
+import os
+
+# List of the folders to look for.
+folders = []
+for file in os.listdir():
+    if os.path.isdir(file):
+        if 'GRB' in file:
+        	folders.append(file)
+        elif 'SN' in file:
+        	folders.append(file)
+
+folders = ['GRB030329-SN2003dh']
+# Loop over the folders.
+for folder in folders:
+
+	# Open the existing text readme. 
+	with open(folder+'/readme.txt', 'r') as readme:
+		readmelines = readme.readlines()
+
+	# Create a markdown doc in the correct folder. 
+	doc = snakemd.new_doc(folder+'/readme.md')
+
+	# 
