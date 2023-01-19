82809daaf2886991fa793be420f0348a9e99a2ed
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2023-01-19 18:06:25 +0000

    Squashed commit of the following:
    commit 749c18c610c9ca402532362893afe5f2aeb0a01a
    Author: GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
    Date:   Thu Jan 19 18:05:20 2023 +0000
    
        Pylinted these pythons

--- Webtool/static/adsapi.py
+++ Webtool/static/adsapi.py
@@ -1,60 +1,63 @@
 '''
-This script needs to be run to return the citation information from NASA ADS. This is the data that eventually appears in the table on the event page. 
+This script needs to be run to return the citation information from NASA ADS.
+This is the data that eventually appears in the table on the event page.
 '''
 
-import numpy as np
+import json
+import sqlite3
+import requests
 import pandas as pd
 
-#The requests module
-import requests
-import json
-
-#Add the bit for the database access:
-import sqlite3
 def get_db_connection():
-	conn = sqlite3.connect('Masterbase.db')
-	conn.row_factory = sqlite3.Row
-	return conn
+    """
+    Connect to the master database.
+    """
+    conn = sqlite3.connect('Masterbase.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
+def bibcode_names(type):
+    """
+    Return ADS urls of the primary sources
+    """
+    conn = get_db_connection()
+    urls1 = conn.execute(f'SELECT DISTINCT({type}) FROM SQLDataGRBSNe')
+    urls2 = conn.execute('SELECT DISTINCT(source) FROM TrigCoords')
+    urls3 = conn.execute('SELECT DISTINCT(source) FROM PeakTimesMags')
+    urls = []
 
-#Return ADS urls of the primary sources
-def bibcode_names():
-	conn = get_db_connection()
-	urls1 = conn.execute('SELECT DISTINCT(PrimarySources) FROM SQLDataGRBSNe')
-	urls2 = conn.execute('SELECT DISTINCT(source) FROM TrigCoords')
-	urls3 = conn.execute('SELECT DISTINCT(source) FROM PeakTimesMags')
-	urls = []
+    for i in urls1:
+        if i not in urls:
+            urls.append(i)
 
-	for i in urls1:
-		if i not in urls:
-			urls.append(i)
+    for i in urls2:
+        if i not in urls:
+            urls.append(i)
 
-	for i in urls2:
-		if i not in urls:
-			urls.append(i)
+    for i in urls3:
+        if i not in urls:
+            urls.append(i)
 
-	for i in urls3:
-		if i not in urls:
-			urls.append(i)
+    bibcodes = []
+    hyperlinks = []
+    randoms = []
+    for i in urls:
 
-	bibcodes = []
-	hyperlinks = []
-	randoms = [] 
-	for i in urls:
-		
-		if str(i[0])[0:10] == 'https://ui':
+        if str(i[0])[0:10] == 'https://ui':
 
-			#Split the bibcode into a list by breaking it each time a / appears
-			bibcodes.append(str(i[0]).split('/')[4].replace('%26', '&'))
-			hyperlinks.append(str(i[0]))
-		#Include anything without https://ui but dont go to ADS
-		else:
-			randoms.append(i[0])
-	conn.close()
-	
-	return bibcodes, hyperlinks, randoms
+            #Split the bibcode into a list by breaking it each time a / appears
+            bibcodes.append(str(i[0]).split('/')[4].replace('%26', '&'))
+            hyperlinks.append(str(i[0]))
+        #Include anything without https://ui but dont go to ADS
+        else:
+            randoms.append(i[0])
+    conn.close()
 
-bibcodes, hyperlinks, randoms = bibcode_names()
+    return bibcodes, hyperlinks, randoms
+
+# Get bibcodes.
+bibcodes, hyperlinks, randoms = bibcode_names('PrimarySources')
+bibcodes2, hyperlinks2, randoms2 = bibcode_names('SecondarySources')
 
 #API Access
 #Code copied from the howto for the ADS API (though some of it is mine too)
@@ -66,56 +69,33 @@ token = 'vs2uU32JWGtrTQwOFtumfDmmDlCFe2QSJ3rwSOvv'
 dictionary = {}
 for i in range(len(bibcodes)):
 
-	bibcode = {"bibcode":[str(bibcodes[i])], "format": "%m %Y"}
-	r = requests.post("https://api.adsabs.harvard.edu/v1/export/custom", \
-	                 headers={"Authorization": "Bearer " + token, "Content-type": "application/json"}, \
-	                 data=json.dumps(bibcode))
-	print(r.json())
+    bibcode = {"bibcode":[str(bibcodes[i])], "format": "%m %Y"}
+    r = requests.post("https://api.adsabs.harvard.edu/v1/export/custom", \
+                     headers={"Authorization": "Bearer " + token, \
+                     "Content-type": "application/json"}, \
+                     data=json.dumps(bibcode))
+    print(r.json())
 
-	#dictionary[str(hyperlinks[i])] = r.json()['export']
-	author_list = r.json()['export']
-	author_split = r.json()['export'].split(',')
+    #dictionary[str(hyperlinks[i])] = r.json()['export']
+    author_list = r.json()['export']
+    author_split = r.json()['export'].split(',')
 
-	dictionary_a={}
-	if len(author_split)>2:
-		dictionary_a['names'] = author_split[0]+' et al.'
-		dictionary_a['year'] = author_list[-5:-1]
-	else:
-		dictionary_a['names'] = author_list[:-6]
-		dictionary_a['year'] = author_list[-5:-1]
-	dictionary[str(hyperlinks[i])] = dictionary_a
+    dictionary_a={}
+    if len(author_split)>2:
+        dictionary_a['names'] = author_split[0]+' et al.'
+        dictionary_a['year'] = author_list[-5:-1]
+    else:
+        dictionary_a['names'] = author_list[:-6]
+        dictionary_a['year'] = author_list[-5:-1]
+    dictionary[str(hyperlinks[i])] = dictionary_a
 
 #Take care of the randoms
 for i in range(len(randoms)):
-	dictionary[randoms[i]] = randoms[i]
+    dictionary[randoms[i]] = randoms[i]
 
 #Save the dictionary with json.dump()
-file = open("citations/citations.json", 'w')
-json.dump(dictionary, file, indent=4, separators=(',', ': '))
-file.close()
-
-#Return ADS urls of the secondary sources
-def secondary_bibcode_names():
-	conn = get_db_connection()
-	urls = conn.execute('SELECT DISTINCT(SecondarySources) FROM SQLDataGRBSNe')
-	bibcodes = []
-	hyperlinks = []
-	randoms = [] 
-	for i in urls:
-		
-		if str(i[0])[0:10] == 'https://ui':
-
-			#Split the bibcode into a list by breaking it each time a / appears
-			bibcodes.append(str(i[0]).split('/')[4].replace('%26', '&'))
-			hyperlinks.append(str(i[0]))
-		#Include anything without https://ui but dont go to ADS
-		else:
-			randoms.append(i[0])
-	conn.close()
-	
-	return bibcodes, hyperlinks, randoms
-
-bibcodes2, hyperlinks2, randoms2 = secondary_bibcode_names()
+with open("citations/citations.json", 'w', encoding='utf-8') as file:
+    json.dump(dictionary, file, indent=4, separators=(',', ': '))
 
 #API Access
 #Code copied from the howto for the ADS API (though some of it is mine too)
@@ -124,39 +104,41 @@ bibcodes2, hyperlinks2, randoms2 = secondary_bibcode_names()
 dictionary2 = {}
 for i in range(len(bibcodes2)):
 
-	bibcode2 = {"bibcode":[str(bibcodes2[i])], "format": "%m %Y"}
-	r = requests.post("https://api.adsabs.harvard.edu/v1/export/custom", \
-	                 headers={"Authorization": "Bearer " + token, "Content-type": "application/json"}, \
-	                 data=json.dumps(bibcode2))
-	print(r.json())
-	# dictionary2[str(hyperlinks2[i])] = r.json()['export']
+    bibcode2 = {"bibcode":[str(bibcodes2[i])], "format": "%m %Y"}
+    r = requests.post("https://api.adsabs.harvard.edu/v1/export/custom", \
+         headers={"Authorization": "Bearer " + token, \
+         "Content-type": "application/json"}, \
+         data=json.dumps(bibcode2))
+    print(r.json())
+    # dictionary2[str(hyperlinks2[i])] = r.json()['export']
 
-	author_list = r.json()['export']
-	author_split = r.json()['export'].split(',')
+    author_list = r.json()['export']
+    author_split = r.json()['export'].split(',')
 
-	dictionary_a={}
-	if len(author_split)>2:
-		dictionary_a['names'] = author_split[0]+' et al.'
-		dictionary_a['year'] = author_list[-5:-1]
-	else:
-		dictionary_a['names'] = author_list[:-6]
-		dictionary_a['year'] = author_list[-5:-1]
-	dictionary2[str(hyperlinks2[i])] = dictionary_a
+    dictionary_a={}
+    if len(author_split)>2:
+        dictionary_a['names'] = author_split[0]+' et al.'
+        dictionary_a['year'] = author_list[-5:-1]
+    else:
+        dictionary_a['names'] = author_list[:-6]
+        dictionary_a['year'] = author_list[-5:-1]
+    dictionary2[str(hyperlinks2[i])] = dictionary_a
 
 #Take care of the randoms
 for i in range(len(randoms2)):
-	dictionary2[randoms2[i]] = randoms2[i]
+    dictionary2[randoms2[i]] = randoms2[i]
 #Save the dictionary with json.dump()
-file = open("citations/citations2.json", 'w')
-json.dump(dictionary2, file, indent=4, separators=(',', ': '))
-file.close()
+with open("citations/citations2.json", 'w', encoding='utf-8') as file:
+    json.dump(dictionary2, file, indent=4, separators=(',', ': '))
 
-# Get the ADS data on the data I downloaded from ADS papers.
+
 def grb_names():
+    """
+    # Get the names of all GRB-SNe in the webtool.
+    """
     conn = get_db_connection()
     names = conn.execute('SELECT DISTINCT GRB, SNe FROM SQLDataGRBSNe')
     grbs = []
-    years = []
     for i in names:
         if str(i[0]) != 'None' and str(i[1]) != 'None':
             if 'AT' in str(i[1]):
@@ -176,47 +158,46 @@ def grb_names():
 
     return grbs
 
-# Go to all the GRB-SN source files and get the citations
+# Go to all the GRB-SN source files and get the names.
 folder_names = grb_names()
 
 # Get the bibcodes and hyperlinks
 bibcodes = []
 hyperlinks = []
 for folder in folder_names:
-	df = pd.read_csv('SourceData/'+str(folder)+'/'+str(folder)+'filesources.csv')
-	citations = df['Reference']
+    df = pd.read_csv('SourceData/'+str(folder)+'/'+str(folder)+'filesources.csv')
+    citations = df['Reference']
 
-	for i in citations:
-		if str(i)[0:10] == 'https://ui':
+    for i in citations:
+        if str(i)[0:10] == 'https://ui':
 
-			#Split the bibcode into a list by breaking it each time a / appears
-			bibcodes.append(str(i).split('/')[4].replace('%26', '&'))
-			hyperlinks.append(str(i))
+            #Split the bibcode into a list by breaking it each time a / appears
+            bibcodes.append(str(i).split('/')[4].replace('%26', '&'))
+            hyperlinks.append(str(i))
 
 
 dictionary3 = {}
 for i in range(len(bibcodes)):
 
-	bibcode = {"bibcode":[str(bibcodes[i])], "format": "%m %Y"}
-	r = requests.post("https://api.adsabs.harvard.edu/v1/export/custom", \
-	                 headers={"Authorization": "Bearer " + token, "Content-type": "application/json"}, \
-	                 data=json.dumps(bibcode))
-	print(r.json())
+    bibcode = {"bibcode":[str(bibcodes[i])], "format": "%m %Y"}
+    r = requests.post("https://api.adsabs.harvard.edu/v1/export/custom", \
+                     headers={"Authorization": "Bearer " + token, \
+                     "Content-type": "application/json"}, \
+                     data=json.dumps(bibcode))
+    print(r.json())
 
-	author_list = r.json()['export']
-	author_split = r.json()['export'].split(',')
+    author_list = r.json()['export']
+    author_split = r.json()['export'].split(',')
 
-	dictionary_a={}
-	if len(author_split)>2:
-		dictionary_a['names'] = author_split[0]+' et al.'
-		dictionary_a['year'] = author_list[-5:-1]
-	else:
-		dictionary_a['names'] = author_list[:-6]
-		dictionary_a['year'] = author_list[-5:-1]
-	dictionary3[str(hyperlinks[i])] = dictionary_a
+    dictionary_a={}
+    if len(author_split)>2:
+        dictionary_a['names'] = author_split[0]+' et al.'
+        dictionary_a['year'] = author_list[-5:-1]
+    else:
+        dictionary_a['names'] = author_list[:-6]
+        dictionary_a['year'] = author_list[-5:-1]
+    dictionary3[str(hyperlinks[i])] = dictionary_a
 
 #Save the dictionary with json.dump()
-file = open("citations/citations(ADSdatadownloads).json", 'w')
-json.dump(dictionary3, file, indent=4, separators=(',', ': '))
-file.close()
-
+with open("citations/citations(ADSdatadownloads).json", 'w', encoding='utf-8') as file:
+	json.dump(dictionary3, file, indent=4, separators=(',', ': '))
