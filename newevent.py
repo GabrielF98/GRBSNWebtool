c1d5782eac488bc26e63975bbf094350a97adbbf
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2023-09-19 19:42:04 +0100

    Created newevent.py
    This will automatically do everything that needs to be done for a new event, create a folder with readmes and filesources csvs and updated the DB.

--- newevent.py
+++ newevent.py
@@ -1,12 +1,13 @@
 '''
 Create a new folder, OriginalFormats, filesources.csv and readme.txt file for a GRB-SN.
-Create a Notion entry for a new GRB-SN.
 '''
 
 import os
 
 import numpy as np
 import pandas as pd
+
+from yaml2db import _load_yaml_config, dict2db
 
 # Find new GRB-SNe
 notion_events = pd.read_csv('Webtool/static/SourceData/tags.csv')
@@ -26,14 +27,49 @@ for event in notion_events['Name']:
         print(f'{event} is missing.')
 
         # Create the folder
+        cwd = os.getcwd()
         os.chdir('Webtool/static/SourceData')
         os.mkdir(event)
         os.chdir(event)
 
         # Create the readme.txt
+        with open('readme.txt', 'w') as file:
+            file.write(f'Event Name: {event}\n')
+            file.write(
+                '=========================================================\n')
+            file.write('Filename:\n')
+            file.write(
+                '---------------------------------------------------------\n')
 
         # Create filesources.csv
+        with open(event+'filesources.csv', 'w') as file:
+            file.write('GRB/SN, Filename, Status, Reference\n')
 
         # Create Original formats folder.
+        os.mkdir('OriginalFormats')
 
-        # Create a blank entry in the DB.
+        # Change back to the right directory
+        os.chdir(cwd)
+
+        # Create a blank entry in the DBs.
+        yaml_db_info = _load_yaml_config('database_entry.yaml')
+        if len(event.split('-')) == 2:
+            yaml_db_info['PeakTimesMags']['sn_name'] = event.split('-')[1][2:]
+            yaml_db_info['PeakTimesMags']['grb_id'] = event.split('-')[0][3:]
+            yaml_db_info['TrigCoords']['sn_name'] = event.split('-')[1][2:]
+            yaml_db_info['TrigCoords']['grb_id'] = event.split('-')[0][3:]
+            yaml_db_info['SQLDataGRBSNe']['GRB'] = event.split('-')[0][3:]
+            yaml_db_info['SQLDataGRBSNe']['SNe'] = event.split('-')[1][2:]
+
+        elif len(event.split('-')) == 1:
+            if event[0] == 'S':
+                yaml_db_info['PeakTimesMags']['sn_name'] = event[2:]
+                yaml_db_info['TrigCoords']['sn_name'] = event[2:]
+                yaml_db_info['SQLDataGRBSNe']['SNe'] = event[2:]
+
+            elif event[0] == 'G':
+                yaml_db_info['PeakTimesMags']['grb_id'] = event[3:]
+                yaml_db_info['TrigCoords']['grb_id'] = event[3:]
+                yaml_db_info['SQLDataGRBSNe']['GRB'] = event[3:]
+
+        dict2db(yaml_db_info)
