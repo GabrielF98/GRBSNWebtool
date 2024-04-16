ed1f9131ffe09263bcc2e4ccb2c3a5f4d0178b39
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2024-04-16 19:27:46 +0100

    Revert "Moved all json files to the OriginalFormats folder."
    This reverts commit 1472e07249c365597dcaf68d326ff53c77a1a4a4.

--- Webtool/static/SourceData/json2grbsnformat.py
+++ Webtool/static/SourceData/json2grbsnformat.py
@@ -1,7 +1,6 @@
 import glob
 import json
 import os
-import shutil
 
 import numpy as np
 import pandas as pd
@@ -161,20 +160,12 @@ for folder in folder_list:
     json_list = glob.glob('*.json')
 
     # Perform operations on all json files
-    # for file in json_list:
-    #     if 'Spectra' in file:
-    #         file_dict = load_json(file)
-    #         all_keys = []
-    #         list_of_keys = find_keys(file_dict)
-    #         convert_key(list_of_keys, file_dict, file)
-
-    # Shuttle all json files to the OriginalFormats folder
     for file in json_list:
-        try:
-            shutil.move(file, 'OriginalFormats/')
-        except FileNotFoundError:
-            os.mkdir('OriginalFormats/')
-            shutil.move(file, 'OriginalFormats/')
+        if 'Spectra' in file:
+            file_dict = load_json(file)
+            all_keys = []
+            list_of_keys = find_keys(file_dict)
+            convert_key(list_of_keys, file_dict, file)
 
     # Return to the folder where you are running this code.
     os.chdir(pwd)
