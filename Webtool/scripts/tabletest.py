d179386d1ac4c0365b9a06d6c9ff7c6f650773b3
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2025-02-20 12:50:40 +0000

    Update tabletest.py
    Moved to scripts/. Modified the script to make it work in the new location.

--- Webtool/scripts/tabletest.py
+++ Webtool/scripts/tabletest.py
@@ -2,8 +2,12 @@
 Prints the dataframe representation of a text file containing GRB-SN data.
 Allows for quick identification of faults with the data standardisation.
 '''
+import os
 import sys
+
 import pandas as pd
+
+SOURCE_DATA_PATH = "../static/SourceData/"
 
 filename = sys.argv[1]
 
@@ -14,5 +18,5 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-df = pd.read_csv(grbsn+'/'+filename, sep='\t')
+df = pd.read_csv(os.path.join(SOURCE_DATA_PATH, grbsn, filename), sep="\t")
 print(df)
