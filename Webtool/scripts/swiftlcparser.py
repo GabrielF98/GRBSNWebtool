c1f6f0909dcff74880071c6e0b1fdd17201003be
GabrielF98 <72733933+GabrielF98@users.noreply.github.com>
2025-02-20 12:40:49 +0000

    Update swiftlcparser.py
    Moved to scripts/. Modified the file to make it work in its new location. Verified that it runs correctly.

--- Webtool/scripts/swiftlcparser.py
+++ Webtool/scripts/swiftlcparser.py
@@ -3,50 +3,69 @@ Converts any Swift xrtlc files to the correct format to be plotted by the tool.
 '''
 
 import os
+
 import numpy as np
 
-root = os.getcwd()
-dirs = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+PATH_TO_SOURCE_DATA = "../static/SourceData/"
+dirs = [
+    item
+    for item in os.listdir(PATH_TO_SOURCE_DATA)
+    if os.path.isdir(os.path.join(PATH_TO_SOURCE_DATA, item))
+]
 
 eventlist = []
 for i in dirs:
     if 'GRB' in str(i) or 'SN' in str(i):
-        eventlist.append(i)
-print(eventlist)
+        eventlist.append(os.path.join(PATH_TO_SOURCE_DATA, i))
+
 # Loop over all folders
 for folder in eventlist:
-    #Check if that folder has an xrtlc
-    filelist = os.listdir(folder)
-    if str(folder).split('-', maxsplit=1)[0]+'xrtlc.txt' in filelist:
-        print(str(folder).split('-', maxsplit=1)[0]+'xrtlc.txt')
-        #Read and write to the file
-        with open(str(folder)+'/'+str(folder).split('-', maxsplit=1)[0]+'xrtlc.txt', 'r') as fnew:
-            #Open and read lines of the file
+    # Check if that folder has an xrtlc
+    if os.path.exists(
+        os.path.join(
+            folder, os.path.basename(folder).split("-")[0] + "xrtlc.txt"
+        )
+    ):
+        print(folder)
+        # Read and write to the file
+        with open(
+            os.path.join(
+                folder, os.path.basename(folder).split("-")[0] + "xrtlc.txt"
+            ),
+            "r",
+        ) as fnew:
+            # Open and read lines of the file
             f = fnew.readlines()
             f = np.array(f)
 
-        #Check if we already fixed this file
+        # Check if we already fixed this file
         if f[0]=='col1\tcol2\tcol3\tcol4\tcol5\tcol6\tcol7\n':
             print("already fixed")
         else:
-            with open(str(folder)+'/'+
-                str(folder).split('-', maxsplit=1)[0]+'xrtlc.txt', 'w', encoding='utf-8') as fnew:
+            with open(
+                os.path.join(
+                    folder,
+                    os.path.basename(folder).split("-")[0] + "xrtlc.txt",
+                ),
+                "w",
+                encoding="utf-8",
+            ) as fnew:
 
-                #Skip the big header line
+                # Skip the big header line
                 start = 0
                 for i in range(len(f)):
                     if 'READ' in str(f[i]):
                         start = i+1
                         print('start is:', start)
 
-                #Write a header line
+                # Write a header line
                 fnew.write('col1\tcol2\tcol3\tcol4\tcol5\tcol6\tcol7\n')
 
                 # Loop through the lines to find what we need:
                 code = 0
 
                 for i in range(start, len(f)):
-                    #make sure its not a 'NO NO NO NO NO' line etc
+                    # make sure its not a 'NO NO NO NO NO' line etc
                     if 'Flux' not in str(f[i]) and 'NO' not in str(f[i]):
                         if 'WTSLEW' in str(f[i]):
                             code = 1
